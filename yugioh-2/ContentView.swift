7f6df8506ef2921626085d9860168616d5388fc7
Şahin Karahan <sahin.karahan@hotmail.com>
2024-12-11 14:56:16 +0300

    Initial Commit

---
+++ yugioh-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yugioh-2
+//
+//  Created by Şahin Karahan on 11.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yugioh-2/yugioh_2App.swift
@@ -0,0 +1,17 @@
+//
+//  yugioh_2App.swift
+//  yugioh-2
+//
+//  Created by Şahin Karahan on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct yugioh_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
